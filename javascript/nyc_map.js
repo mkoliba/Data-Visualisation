e83ad4b00644959379dc21962012ea03492f499e
martin <martin.koliba@gmail.com>
2018-05-12 01:17:13 +0200

    Parsed data and draw basic points of the stations

--- javascript/nyc_map.js
+++ javascript/nyc_map.js
@@ -4,27 +4,29 @@ var mapwidth = 900,
 var projection;
 
 var mapsvg = d3.select("#nyc_map")
-    .attr("id", "Map")
     .attr("width", mapwidth)
-    .attr("height", mapheight) 
-    ;
+    .attr("height", mapheight);
 
-    g = mapsvg.append("g");
-
+var zoom_group = mapsvg.append("g");	
+var map_group = zoom_group.append("g");
+var projection = d3.geoMercator();
+var path
 
 d3.json("data/NYC_MapInfo.geojson", function(error, NYC_MapInfo) {
+	//.log(projection.scale());
+	
+	projection.center([40.774986, -73.946488]).angle([-55]).fitExtent([[-200, 0],[mapwidth+200, mapheight+500]], NYC_MapInfo);
+	
+	//console.log(projection.translate());
 
+	//projection.scale(89382.38598763589);
 
-    var projection = d3.geoMercator().rotate([0, -55, 0]).fitSize([mapwidth, mapheight*1.7], NYC_MapInfo);
+	//console.log(projection.scale());
 
-    var path = d3.geoPath().projection(projection);
+    path = d3.geoPath().projection(projection);
     
-
-      
 	
-	g = mapsvg.append("g");
-	
-	g.selectAll("path")
+	map_group.selectAll("path")
 		.data(NYC_MapInfo.features)
 		.enter()
 		.append("path")
@@ -51,11 +53,11 @@ d3.json("data/NYC_MapInfo.geojson", function(error, NYC_MapInfo) {
 		.on("mouseover",function(d) {
 			d3.select(this).style("fill-opacity","1");
 		})
-		.on("mouseout",function(d) {
-			d3.selectAll("path").style("fill-opacity","0.7");
-		});
+		// .on("mouseout",function(d) {
+		// 	d3.selectAll("path").style("fill-opacity","0.7");
+		// });
 		
-	g.selectAll("text")
+	map_group.selectAll("text")
 	.data(NYC_MapInfo.features)
 	.enter().append("svg:text")
 	.attr("dy",".35em")
@@ -65,7 +67,7 @@ d3.json("data/NYC_MapInfo.geojson", function(error, NYC_MapInfo) {
 	})
 	.style("text-anchor","middle");
     
-    
+	draw_points()
     
 });
 
@@ -75,7 +77,44 @@ var zoom_handler = d3.zoom()
 
 //specify what to do when zoom event listener is triggered 
 function zoom_actions(){
- g.attr("transform", d3.event.transform);
+	
+	zoom_group.attr("transform", d3.event.transform);
+	//console.log(projection.scale())
 }
 
 zoom_handler(mapsvg);
+
+//##################################################################
+
+
+var point_group = zoom_group.append("g");
+
+
+function draw_points(){
+	d3.json("data/stations_rents_outin.json", function(error, rents_out_in) {
+		console.log(rents_out_in)
+		
+		point_group.selectAll("circle")
+			.data(rents_out_in.stations)
+			.enter()
+			.append("circle")
+				.attr("cx", function(d) {
+					if (d && d.lon && d.lat) 
+						 return projection([d.lon, d.lat])[0]; })
+				.attr("cy", function(d) {
+					if (d && d.lon && d.lat) return projection([d.lon, d.lat])[1];
+				})
+				.attr("r", 3)
+				//.attr("class", "non_brushed")
+				.style("z-index", 4)
+				.style("position", "relative")
+				.style("fill", "yellow")
+				.style("stroke", "gray")
+				.style("stroke-width", 0.25)
+				.style("opacity", 0.75)
+			.append("title") 
+				.text(function(d) {
+					return d.name + " " + d.lon + " " + d.lat; });
+	})
+}
+
