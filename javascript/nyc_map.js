1892a205329e6381e424f766db6ee11f21ff53f2
martin <martin.koliba@gmail.com>
2018-05-12 18:07:13 +0200

    Added support for colorring potins

--- javascript/nyc_map.js
+++ javascript/nyc_map.js
@@ -8,9 +8,10 @@ var mapsvg = d3.select("#nyc_map")
     .attr("height", mapheight);
 
 var zoom_group = mapsvg.append("g");	
-var map_group = zoom_group.append("g");
+var nyc_map_group = zoom_group.append("g");
+var njc_map_group = zoom_group.append("g");
 var projection = d3.geoMercator();
-var path
+var path;
 
 d3.json("data/NYC_MapInfo.geojson", function(error, NYC_MapInfo) {
 	//.log(projection.scale());
@@ -26,7 +27,7 @@ d3.json("data/NYC_MapInfo.geojson", function(error, NYC_MapInfo) {
     path = d3.geoPath().projection(projection);
     
 	
-	map_group.selectAll("path")
+	nyc_map_group.selectAll("path")
 		.data(NYC_MapInfo.features)
 		.enter()
 		.append("path")
@@ -57,7 +58,7 @@ d3.json("data/NYC_MapInfo.geojson", function(error, NYC_MapInfo) {
 		// 	d3.selectAll("path").style("fill-opacity","0.7");
 		// });
 		
-	map_group.selectAll("text")
+	nyc_map_group.selectAll("text")
 	.data(NYC_MapInfo.features)
 	.enter().append("svg:text")
 	.attr("dy",".35em")
@@ -66,8 +67,8 @@ d3.json("data/NYC_MapInfo.geojson", function(error, NYC_MapInfo) {
 		return "translate(" + path.centroid(d)[0] + "," + path.centroid(d)[1] + ")";
 	})
 	.style("text-anchor","middle");
-    
-	draw_points()
+    draw_NJC_map();
+	draw_points();
     
 });
 
@@ -84,16 +85,66 @@ function zoom_actions(){
 
 zoom_handler(mapsvg);
 
+function draw_NJC_map (){
+	d3.json("data/jerseycityzipcodegeojson.geojson", function(error, njc_map_data) {
+		
+		njc_map_group.selectAll("path")
+		.data(njc_map_data.features)
+		.enter()
+		.append("path")
+			.attr("d", path)
+		.style("fill", "#CCC")
+		.style("fill-opacity","0.7")
+		// .on("mouseover",function(d) {
+		// 	d3.select(this).style("fill-opacity","1");
+		// })
+	
+	
+	});
+}
+
 //##################################################################
 
 
 var point_group = zoom_group.append("g");
 
+var station_data;
+
+function relative_difference(out_r, in_r){
+	if (out_r == 0 && in_r == 0){
+		return 0
+	}
+	//console.log((out_r - in_r)/(d3.max([out_r, in_r])));
+	return (out_r - in_r)/(d3.max([out_r, in_r]));
+}
+
+function toPaddedHexString(num, len) {
+	num = Math.floor(num);
+	var str = num.toString(16);
+    return "0".repeat(len - str.length) + str;
+}
+
+console.log(relative_difference[7,8])
+console.log(relative_difference[7,8])
+
+function color_scale(x){
+	var red, blue;
+	if (x>0){
+		blue = 255;
+		red = 255 - 255*x ;
+	}
+	else{
+		red = 255;
+		blue = 255 + 255 * x ;
+	}
+	rgb = '#' + toPaddedHexString(red,2) + '00' + toPaddedHexString(blue,2);
+	return rgb;
+}
 
 function draw_points(){
 	d3.json("data/stations_rents_outin.json", function(error, rents_out_in) {
-		console.log(rents_out_in)
-		
+		//console.log(rents_out_in)
+		station_data =rents_out_in;
 		point_group.selectAll("circle")
 			.data(rents_out_in.stations)
 			.enter()
@@ -114,7 +165,32 @@ function draw_points(){
 				.style("opacity", 0.75)
 			.append("title") 
 				.text(function(d) {
-					return d.name + " " + d.lon + " " + d.lat; });
-	})
+					return d.name; });
+
+
+			update_poits();
+	});
+
+
+}
+
+
+function update_poits(){
+	//console.log(station_data.values[0])
+
+	out_in = station_data.values[100]
+	
+	point_group.selectAll("circle")
+		.data(out_in)
+		.style("fill", function(d){
+			//console.log(d,(d[0] - d[1])/(d3.max([d[0], d[1]])),color_scale(relative_difference(d[0],d[1])))
+			return color_scale(relative_difference(d[0],d[1]));
+		} )
+		.selectAll("title")
+    		.text(function(d,i){
+				//console.log(i, out_in[d.o][0])
+				return d.name + "in: " + out_in[d.o][1] + " out: " + out_in[d.o][0];
+			});
+		
 }
 
